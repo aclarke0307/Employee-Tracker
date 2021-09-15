95bd17cf64b3180a110c8b4e4fe568b75655033d
aclar <a.clarke0307@gmail.com>
2021-09-15 18:11:44 -0400

    start function

--- server.js
+++ server.js
@@ -14,3 +14,50 @@ connection.connect(function(err){
     if(err) throw err;
     start();
 });
+// inquirer start function
+function start(){
+    inquirer
+    .prompt({
+        type: "list",
+        name: "option",
+        message: "What would you like to do?",
+        choices: [
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "view Department",
+            "view Role",
+            "view Employee",
+            "update Employee Role",
+            "Exit"
+        ]
+    })
+    .then(function(result){
+        switch (result.option){
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "view Department":
+                viewDepartment();
+                break;
+            case "view Role":
+                viewRole();
+                break;
+            case "view Employee":
+                viewEmployee();
+                break;
+            case "update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+        }
+    });
+}
